ada34ad1b66c781f88f9c620ca22ea0948510b6e
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-04-15 08:45:26 -0300

    chains_under_the_hood exemplo de código alternativo adicionado

--- Ollama/3_chains/2_chains_under_the_hood.py
+++ Ollama/3_chains/2_chains_under_the_hood.py
@@ -21,11 +21,23 @@ format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
 invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
 parse_output = RunnableLambda(lambda x: x.content)
 
+# Dica de um usuário: pra deixar um pouco mais intuitivo, podemos fazer assim:
+# invoke_format = RunnableLambda(lambda x: prompt_template.invoke(x))
+# invoke_model = RunnableLambda(lambda x: model.invoke(x))
+# parser_output = RunnableLambda(lambda x: StrOutputParser().invoke(x))
+# chain = RunnableSequence(invoke_format, invoke_model, parser_output)
+
 # Criação da cadeia de Sequência utilizando o RunnableSequence
 # É equivalente à implementação anterior, com LCEL
 chain = RunnableSequence(
     first=format_prompt, middle=[invoke_model], last=parse_output
 )
+
+# Poderíamos trocar todo o código acima pelo exemplo anterior:
+# Criar a cadeia combinada usando LangChain Expression Language (LCEL)
+# chain = (
+#     prompt_template | model | StrOutputParser()
+# )  # separa apenas o conteúdo e devolve em forma de string
 
 # Execução da cadeia
 response = chain.invoke({'assunto': 'cachorros', 'quantidade_piadas': '3'})
