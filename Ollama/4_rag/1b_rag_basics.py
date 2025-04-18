4d9fd45e5191a5a045f4fbd2682d5698a3b201fd
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-04-18 11:47:56 -0300

    rag básico parte 2

---
+++ Ollama/4_rag/1b_rag_basics.py
@@ -0,0 +1,39 @@
+# Importações necessárias
+import os
+
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+from ollama import embeddings
+
+# Definindo o caminho do diretório de persistência
+current_dir = os.path.dirname(os.path.abspath(__file__))
+persistent_directory = os.path.join(current_dir, 'db', 'chroma_db')
+
+# Define o modelo de Embedding
+embeddings = OllamaEmbeddings(model='nomic-embed-text')
+
+# Carrega o vector store existente com a função de embedding
+db = Chroma(
+    persist_directory=persistent_directory, embedding_function=embeddings
+)
+
+# Define a pergunta do usuário
+query = 'Who is Odysseys wife?'
+
+# Cria um retriever para recuperar os documentos mais relevantes
+# e define o limiar de similaridade
+retriever = db.as_retriever(
+    search_type='similarity_score_threshold',
+    search_kwargs={'k': 3, 'score_threshold': 0.4},
+)
+
+# Recupera os documentos mais relevantes com base na pergunta
+relevant_docs = retriever.invoke(query)
+
+# Imprime os documentos relevantes
+print(f'\n--- Relevant Documents ---')
+for i, doc in enumerate(relevant_docs, 1):
+    print(f'Document {i}:\n{doc.page_content}\n')
+    if doc.metadata:
+        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
